15960cbf79d6bbafcc16b97ab93634748c0ba857
schnabear <schnabear@gmail.com>
2016-03-11 09:19:09 +0800

    Added love for word count exercise

---
+++ wordcount.py
@@ -0,0 +1,92 @@
+"""Wordcount exercise
+
+The main() below is already defined and complete. It calls print_words()
+and print_top() functions which you write.
+
+1. For the --count flag, implement a print_words(filename) function that counts
+how often each word appears in the text and prints:
+word1 count1
+word2 count2
+...
+
+Print the above list in order sorted by word (python will sort punctuation to
+come before letters -- that's fine). Store all the words as lowercase,
+so 'The' and 'the' count as the same word.
+
+2. For the --topcount flag, implement a print_top(filename) which is similar
+to print_words() but which prints just the top 20 most common words sorted
+so the most common word is first, then the next most common, and so on.
+
+Use str.split() (no arguments) to split on all whitespace.
+
+Workflow: don't build the whole program at once. Get it to an intermediate
+milestone and print your data structure and sys.exit(0).
+When that's working, try for the next milestone.
+
+Optional: define a helper function to avoid code duplication inside
+print_words() and print_top().
+
+"""
+
+import sys
+
+# +++your code here+++
+# Define print_words(filename) and print_top(filename) functions.
+# You could write a helper utility function that reads a file
+# and builds and returns a word/count dict for it.
+# Then print_words() and print_top() can just call the utility function.
+
+def count_words(filename):
+    fh = open(filename, 'rU')
+    contents = fh.read()
+    fh.close()
+    word_count = {}
+    words = contents.lower().replace("\n", ' ').split()
+    for word in words:
+        if not word.isalpha():
+            continue
+        if word in word_count.keys():
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+    return word_count
+
+def print_words(filename):
+    word_count = count_words(filename)
+    for word, count in word_count.items():
+        print(word, count) 
+
+def print_top(filename):
+    word_count = count_words(filename)
+    word_count = [[word, count] for word, count in word_count.items()]
+    word_count = sorted(word_count, reverse=True, key=get_word_count)
+    counter = 0
+    for word, count in word_count:
+        print(word, count)
+        counter += 1
+        if counter >= 20:
+            break;
+
+def get_word_count(a):
+    return a[-1]
+
+# This basic command line argument parsing code is provided and
+# calls the print_words() and print_top() functions which you must define.
+def main():
+    if len(sys.argv) != 3:
+        print('usage: ./wordcount.py {--count | --topcount} file')
+        sys.exit(1)
+
+    option = sys.argv[1]
+    filename = sys.argv[2]
+    if option == '--count':
+        print_words(filename)
+    elif option == '--topcount':
+        print_top(filename)
+    else:
+        print('unknown option: ' + option)
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
